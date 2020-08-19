5e9b25a4c56fb95032d72567af67b0e8d9a150fe
nowLetsgo <lipeihua0922@163.com>
2020-08-19 15:59:20 +0800

    8.19下午

---
+++ day04/08.常见的MIME类型.js
@@ -0,0 +1,18 @@
+/* 
+    常见的MIME的类型
+        - text/plain   文本类型
+        - text/html html
+        - text/css  css
+        - application/javascript  js
+        - application/json json
+        - image/jpg
+        - image/jpeg
+        - image/png
+        - image/gif
+        - image/webp webp图片
+        - audio/mp3 音频
+        - video/mp4 视频
+        - application/x-www-form-urlencoded form表单
+
+*/
+response.setHeader("Content-Type", "application/javascript;charset=utf-8");
